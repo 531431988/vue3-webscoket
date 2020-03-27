7a3511fd43a6586b09643c6dc68bef8661feb8fb
wm <531431988@qq.com>
2020-03-27 15:40:02 +0800

    修复图片压缩

--- vue.config.js
+++ vue.config.js
@@ -27,24 +27,19 @@ module.exports = {
       .set('vue$', 'vue/dist/vue.esm.js')
       .set('@', resolve('src')) // key,value自行定义，比如.set('@@', resolve('src/components'))
       .set('@ant-design/icons/lib/dist$', resolve('./icons.js'))
-    // 压缩图片
-    config.module
-      .rule('images')
-      .use('image-webpack-loader')
-      .loader('image-webpack-loader')
-      .options({
-        mozjpeg: { progressive: true, quality: 65 },
-        optipng: { enabled: false },
-        pngquant: { quality: '65-90', speed: 4 },
-        gifsicle: { interlaced: false },
-        webp: { quality: 75 }
-      })
     if (env) {
-      config.plugin('webpack-report').use(BundleAnalyzerPlugin, [
-        {
-          analyzerMode: 'static'
-        }
-      ])
+      // 压缩图片
+      config.module
+        .rule('images')
+        .use('image-webpack-loader')
+        .loader('image-webpack-loader')
+        .options({
+          mozjpeg: { progressive: true, quality: 65 },
+          optipng: { enabled: false },
+          pngquant: { quality: [0.65, 0.9], speed: 4 },
+          gifsicle: { interlaced: false }
+          // webp: { quality: 75 }
+        })
     }
     return config
   },
